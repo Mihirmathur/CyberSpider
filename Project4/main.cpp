02c7e36ebae5cdbbda952fff80a189d4bbdeaeae
Mihirmathur <mihirmathur@g.ucla.edu>
2016-03-05 22:47:27 -0800

    Initial Commit - Skeleton Files

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,15 +1,11 @@
-//
-//  main.cpp
-//  Project4
-//
+//  Cyber Spider
+//  CS32 - Project4
 //  Created by Mihir Mathur on 3/5/16.
 //  Copyright © 2016 Mihir Mathur. All rights reserved.
-//
+
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(){
+    
 }
